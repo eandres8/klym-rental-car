581bd6258ab8d620862750f6ed92e89cb41b3fc8
Andres Vega <elb.andres8@gmail.com>
2022-10-04 14:35:27 -0500

    Implement custom input

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 const moduleList = [
   MatToolbarModule,
-  MatIconModule
+  MatIconModule,
+  MatInputModule,
 ];
 
 @NgModule({
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { DetailsPageComponent } from './details-page/details-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { MaterialModule } from 'src/app/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { InputFilterComponent } from './components/input-filter/input-filter.component';
 
 @NgModule({
   declarations: [
     DetailsPageComponent,
-    MainPageComponent
+    MainPageComponent,
+    InputFilterComponent
   ],
   imports: [
     CommonModule,
