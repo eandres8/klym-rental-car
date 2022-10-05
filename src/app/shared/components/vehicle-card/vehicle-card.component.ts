ee8e66eef3c3002e8ccdb888de013316967675c9
Andres Vega <elb.andres8@gmail.com>
2022-10-05 15:38:34 -0500

    add redirection to details

--- src/app/shared/components/vehicle-card/vehicle-card.component.ts
+++ src/app/shared/components/vehicle-card/vehicle-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ICar } from 'src/app/core/entities/interfaces/car.interface';
 
@@ -7,13 +8,17 @@ import { ICar } from 'src/app/core/entities/interfaces/car.interface';
   templateUrl: './vehicle-card.component.html',
   styleUrls: ['./vehicle-card.component.scss']
 })
-export class VehicleCardComponent implements OnInit {
+export class VehicleCardComponent {
 
   @Input() vehicle: ICar | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private readonly router: Router,
+  ) { }
+  
+  public redirectTo() {
+    console.log(`/details/${this.vehicle?.id}`);
+    this.router.navigate([`/details/${this.vehicle?.id}`]);
   }
 
 }
